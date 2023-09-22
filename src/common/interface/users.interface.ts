a2fc6c29d76c201af6b34823626cc5b6dc26bc19
Lukas <lukas_flores.8@hotmail.com>
2023-09-21 21:24:44 -0300

    install dependencies and users module

---
+++ src/common/interface/users.interface.ts
@@ -0,0 +1,6 @@
+export interface IUsers extends Document {
+  name: string;
+  email: string;
+  password: string;
+  username: string;
+}
---
+++ src/users/dto/users.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class UsersDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+}
